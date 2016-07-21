a9c47d433e58b40386e04d0239672cbba581c50b
walter beller-morales <wbellermorales@fuzzproductions.com>
2016-07-21 02:51:48 -0400

    add styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './examples/mortar_playground/main.js',
+    entry: './examples/flux-todo/main.js',
     output: {
         path: path.join(__dirname, '/build'),
         filename: 'index.js'
